30f075bed1f2bc657568f7e936db829c794099a2
tisma <tisma@gmail.com>
2023-01-07 14:25:22 +0100

    try to place the button at the bottom right

--- tomake/ContentView.swift
+++ tomake/ContentView.swift
@@ -2,45 +2,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+        
+            ZStack(alignment: .top){
                 Color.black
-                    // .ignoresSafeArea()
-            
-            Text("Mathis Zerari")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-        }
+                    .ignoresSafeArea()
+                
+                Rectangle()
+                    .frame(height: 200)
+                    .background(.blue)
+                Text("To Make")
+                    .padding(.horizontal, 136)
+                    .padding(.top, 20)
+                    .padding(.bottom, 25)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .background(.blue)
+                
+                Button {
+                    print("Hello World")
+                } label: {
+                    ZStack(alignment: .bottomTrailing, content: {
+                        Text("+")
+                            .font(.system(size: 80))
+                            .frame(width: 100, height: 700)
+                            .padding(.bottom, 10)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color.white)
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                    })
+                }
+
+            }
     }
 }
 
 struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack(alignment: .top){
-            Color.black
-                .ignoresSafeArea()
-            
-            Rectangle()
-                .frame(height: 200)
-                .background(.blue)
-            Text("To Make")
-                .padding(.horizontal, 136)
-                .padding(.top, 20)
-                .padding(.bottom, 25)
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .background(.blue)
-            
-            Button(action: {
-                print("Round Action")
-            }) {
-                Text("Press")
-                    .frame(width: 200, height: 700)
-                    .foregroundColor(Color.green)
-                    .background(Color.red)
-                    .clipShape(Circle())
-            }
-        }
+        ContentView()
     }
 }
