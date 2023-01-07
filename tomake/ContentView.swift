26edb44ef372e19e79dcb73cf8d98f477b9c0c40
tisma <tisma@gmail.com>
2023-01-07 11:33:55 +0100

    first commit

--- tomake/ContentView.swift
+++ tomake/ContentView.swift
@@ -1,26 +1,48 @@
-//
-//  ContentView.swift
-//  tomake
-//
-//  Created by Mathis Zerari on 31/12/2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+                Color.black
+                    // .ignoresSafeArea()
+            
+            Text("Mathis Zerari")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ZStack(alignment: .top){
+            Color.black
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .frame(height: 200)
+                .background(.blue)
+            Text("To Make")
+                .padding(.horizontal, 136)
+                .padding(.top, 20)
+                .padding(.bottom, 25)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .background(.blue)
+        }
+        
+        // Create a button to add a task
+        
+        Button(action: {
+                    print("Round Action")
+                    }) {
+                    Text("Press")
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.black)
+                        .background(Color.red)
+                        .clipShape(Circle())
+                }
     }
 }
